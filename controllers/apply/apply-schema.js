2a2a2203e4e6ef0d7023a5c5f882ecdbc9aafe0b
Yi Wang <yi@YideMacBook-Pro.local>
2022-12-11 11:12:18 -0500

    update apply

--- controllers/apply/apply-schema.js
+++ controllers/apply/apply-schema.js
@@ -3,6 +3,6 @@ import JobModel from '../jobs/jobs-model.js';
 const schema = mongoose.Schema({
     job: {type: Schema.Types.ObjectId, ref: "jobModel"},
     applyBy: {type: Schema.Types.ObjectId, ref: "userModel"},
-    accept: {type: Boolean, default: false}
+    accept: {type: String, default: "pending"}
 }, {collection: 'applys'});
 export default schema;
